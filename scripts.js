f8822dab73c68d88472d0f99063e1d9ecc4b406a
Alec M <alecmcgovern444@gmail.com>
2015-10-09 23:17:05 -0700

    ship moving properly

--- scripts.js
+++ scripts.js
@@ -1,43 +1,74 @@
 $('document').ready(function(){
 
-	//This code renders the ship to the canvas
- 	
+
 
 	//This Code controls the arrow key movement of the player's ship
+
 	var position = $('#ship').position();
 	var speed = 5;
-	var c = document.getElementById('canvas');
-	var ctx = c.getContext("2d");
 
 	kd.run(function () {
-  		kd.tick();
+ 	 		kd.tick();
 	});
 
-	// kd.DOWN.down(function(){
-	// 	console.log('key down pressed');
-	// 	$('#ship').finish().animate({top: "+="+speed});
-	// });
-	// kd.UP.down(function(){
-	// 	console.log('key down pressed');
-	// 	$('#ship').finish().animate({top: "-="+speed});
-	// });
-	// kd.LEFT.down(function(){
-	// 	console.log('key down pressed');
-	// 	$('#ship').finish().animate({left: "-="+speed});
-	// });
-	// kd.RIGHT.down(function(){
-	// 	console.log('key down pressed');
-	// 	$('#ship').finish().animate({left: "+="+speed});
-	// });
 
-	var spaceShipImage = new Image();
-	spaceShipImage.src = "spaceship_transparent.png"
+	kd.DOWN.down(function(){
+		console.log('key down pressed');
+		$('#ship').finish().animate({top: "+="+speed});
+	});
+	kd.UP.down(function(){
+		console.log('key down pressed');
+		$('#ship').finish().animate({top: "-="+speed});
+	});
+	kd.LEFT.down(function(){
+		console.log('key down pressed');
+		$('#ship').finish().animate({left: "-="+speed});
+	});
+	kd.RIGHT.down(function(){
+		console.log('key down pressed');
+		$('#ship').finish().animate({left: "+="+speed});
+	});
 
-	var render = function(){
-		ctx.drawImage(spaceShipImage, 0, 0, 75, 50);
-	};
 
-	setInterval(render, 10);
+
+
+
+	// var events = {};
+	// var spaceship = {x:0, y:0, speed: 1};
+
+	// var spaceShipImage = new Image();
+	// // var background = new Image();
+	// spaceShipImage.src = "spaceship_transparent.png";
+	// // background.src = "transparent_background.png";
+
+
+	// document.addEventListener("keydown", function(e){events[e.keyCode]=true;}, false);
+	// document.addEventListener("keyup", function(e){delete events[e.keyCode]}, false);
+
+	
+
+	// var render = function(){
+	// 	c.clearRect(0, 0, c.width, c.height);
+	// 	ctx.drawImage(spaceShipImage, spaceship.x, spaceship.y, 75, 50);
+
+	// 	if(37 in events){
+	// 		spaceship.x -= spaceship.speed;
+	// 	}
+	// 	if(38 in events){
+	// 		spaceship.y -= spaceship.speed;
+	// 	}
+	// 	if(39 in events){
+	// 		spaceship.x += spaceship.speed;
+	// 	}
+	// 	if(40 in events){
+	// 		spaceship.y += spaceship.speed;
+	// 	}
+
+
+
+	// };
+
+	// setInterval(render, 10);
 
 
 
