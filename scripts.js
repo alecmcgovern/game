2262432241896a77e95bb0a550d6f70869102403
Alec M <alecmcgovern444@gmail.com>
2015-10-10 22:48:28 -0700

    players can't move offscreen

--- scripts.js
+++ scripts.js
@@ -2,51 +2,69 @@ $('document').ready(function(){
 
 
 
-	//This Code controls the arrow key movement of player  one's ship
+	//This Code controls the movement of the ships
 
 	var speed1 = 5;
 
+
+	//This bit of code is needed to use the kd ("keydrown") attached library
+	// found at https://jeremyckahn.github.io/keydrown/
 	kd.run(function () {
  	 	kd.tick();
 	});
 
+	//This Code controls the arrow key movement of player  one's ship
 
 	kd.DOWN.down(function(){
-		$('#ship1').finish().animate({top: "+="+speed1});
+		if($('#ship1').position().top < 600) {
+			$('#ship1').finish().animate({top: "+="+speed1});
+		}
 	});
 	kd.UP.down(function(){
-		$('#ship1').finish().animate({top: "-="+speed1});
+		if($('#ship1').position().top > 0) {
+			$('#ship1').finish().animate({top: "-="+speed1});
+		}
 	});
 	kd.LEFT.down(function(){
-		$('#ship1').finish().animate({left: "-="+speed1});
+		if($('#ship1').position().left > 0) {
+			$('#ship1').finish().animate({left: "-="+speed1});
+		}
 	});
 	kd.RIGHT.down(function(){
-		$('#ship1').finish().animate({left: "+="+speed1});
+		if($('#ship1').position().left < screen.width-100) {
+			$('#ship1').finish().animate({left: "+="+speed1});
+		}
 	});
-
 
 
 	//This Code controls the 'WASD' movement of player two's ship
 	var speed2 = 5;
 
 	kd.S.down(function(){
-		$('#ship2').finish().animate({top: "+="+speed2});
+		if($('#ship2').position().top < 600) {
+			$('#ship2').finish().animate({top: "+="+speed2});
+		}
 	});
 	kd.W.down(function(){
-		$('#ship2').finish().animate({top: "-="+speed2});
+		if($('#ship2').position().top > 0) {
+			$('#ship2').finish().animate({top: "-="+speed1});
+		}
 	});
 	kd.A.down(function(){
-		$('#ship2').finish().animate({left: "-="+speed2});
+		if($('#ship2').position().left > 0) {
+			$('#ship2').finish().animate({left: "-="+speed1});
+		}
 	});
 	kd.D.down(function(){
-		$('#ship2').finish().animate({left: "+="+speed2});
+		if($('#ship2').position().left < screen.width-100) {
+			$('#ship2').finish().animate({left: "+="+speed1});
+		}
 	});
 
 	
 
-
 	//This code fires the lasers
-	var laser_speed = 10;
+	var laser_speed = 20;
 
 	$(document).keydown(function(e){
 		e.preventDefault();
@@ -60,30 +78,39 @@ $('document').ready(function(){
 	//ship1 laser 
 	function fire1(){
 		var x = $('#ship1').position();
-		$("body").append($("<div>").addClass("laser").css({top: x.top, left: x.left + 50}));
+		$("body").append($("<div>").addClass("laser").css({top: x.top, left: x.left + 36}));
 	}
 
 	//ship 2 laser
 	function fire2(){
 		var y = $('#ship2').position();
-		$("body").append($("<div>").addClass("laser").css({top: y.top, left: y.left + 50}));
+		$("body").append($("<div>").addClass("laser").css({top: y.top, left: y.left + 36}));
 	}
 
 	//udate all laser positions
-	setInterval(updateLaserPosition, 30);
+	setInterval(updateLaserPosition, 20);
 
 	function updateLaserPosition(){
 		$('.laser').each(function(){
 			var oldHeight = $(this).offset().top;
 			if (oldHeight>0){
 				$(this).css("top", oldHeight-laser_speed);
+				var laserCoordinates = $(this).position();
+				console.log(laserCoordinates);
+				if(checkCollision(laserCoordinates)){
+					$(this).remove();
+				}
 			}else{
 				$(this).remove();
 			}
-
 		});
 	}
 	
-
+	//Code to sense when the laser hits a ship
+	function checkCollision(object){
+		//for each alien with class active, check laser coordinates.  
+		// If touching an alien, make it dissapear and return true
+		// If not return false
+	}
 
 });
