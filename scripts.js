8f646a5a5ad74ed8488371a45dd3a2b03fe7358a
Alec M <alecmcgovern444@gmail.com>
2015-10-10 19:20:44 -0700

    lasers working for ship1

--- scripts.js
+++ scripts.js
@@ -4,7 +4,6 @@ $('document').ready(function(){
 
 	//This Code controls the arrow key movement of player  one's ship
 
-	var position = $('#ship1').position();
 	var speed1 = 5;
 
 	kd.run(function () {
@@ -13,88 +12,65 @@ $('document').ready(function(){
 
 
 	kd.DOWN.down(function(){
-		console.log('key down pressed');
 		$('#ship1').finish().animate({top: "+="+speed1});
 	});
 	kd.UP.down(function(){
-		console.log('key down pressed');
 		$('#ship1').finish().animate({top: "-="+speed1});
 	});
 	kd.LEFT.down(function(){
-		console.log('key down pressed');
 		$('#ship1').finish().animate({left: "-="+speed1});
 	});
 	kd.RIGHT.down(function(){
-		console.log('key down pressed');
 		$('#ship1').finish().animate({left: "+="+speed1});
 	});
 
 
 
 	//This Code controls the 'WASD' movement of player two's ship
-	var position = $('#ship2').position();
 	var speed2 = 5;
 
-
-
 	kd.S.down(function(){
-		console.log('key down pressed');
 		$('#ship2').finish().animate({top: "+="+speed2});
 	});
 	kd.W.down(function(){
-		console.log('key down pressed');
 		$('#ship2').finish().animate({top: "-="+speed2});
 	});
 	kd.A.down(function(){
-		console.log('key down pressed');
 		$('#ship2').finish().animate({left: "-="+speed2});
 	});
 	kd.D.down(function(){
-		console.log('key down pressed');
 		$('#ship2').finish().animate({left: "+="+speed2});
 	});
 
-
-
-
-
-	// var events = {};
-	// var spaceship = {x:0, y:0, speed: 1};
-
-	// var spaceShipImage = new Image();
-	// // var background = new Image();
-	// spaceShipImage.src = "spaceship_transparent.png";
-	// // background.src = "transparent_background.png";
-
-
-	// document.addEventListener("keydown", function(e){events[e.keyCode]=true;}, false);
-	// document.addEventListener("keyup", function(e){delete events[e.keyCode]}, false);
-
 	
 
-	// var render = function(){
-	// 	c.clearRect(0, 0, c.width, c.height);
-	// 	ctx.drawImage(spaceShipImage, spaceship.x, spaceship.y, 75, 50);
 
-	// 	if(37 in events){
-	// 		spaceship.x -= spaceship.speed;
-	// 	}
-	// 	if(38 in events){
-	// 		spaceship.y -= spaceship.speed;
-	// 	}
-	// 	if(39 in events){
-	// 		spaceship.x += spaceship.speed;
-	// 	}
-	// 	if(40 in events){
-	// 		spaceship.y += spaceship.speed;
-	// 	}
+	//This code fires the lasers
+	var laser_speed = 10;
 
+	$(document).keydown(function(e){
+		e.preventDefault();
+		if (e.which === 32){
+			console.log("space");
+			fire1();
+		}
+	});
 
+	function fire1(){
+		var x = $('#ship1').position();
+		$("body").append($("<div>").addClass("laser").css({top: x.top, left: x.left + 50}));
+	}
+	setInterval(updateLaserPosition, 30);
 
-	// };
+	function updateLaserPosition(){
+		$('.laser').each(function(){
+			var oldHeight = $(this).offset().top;
+			$(this).css("top", oldHeight-laser_speed);
+		});
+	}
+	
 
-	// setInterval(render, 10);
-
+	
 
 
 
